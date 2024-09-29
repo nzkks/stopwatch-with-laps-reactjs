628cae726c2d16da7443a93b88a51bc569b96929
nzkks <kkshanthosh@gmail.com>
2024-09-29 21:09:20 +1300

    Add all buttons

--- src/components/stopwatch/Stopwatch.tsx
+++ src/components/stopwatch/Stopwatch.tsx
@@ -1,5 +1,17 @@
 const Stopwatch = () => {
-  return <div>Stopwatch</div>;
+  return (
+    <div>
+      <h1>00:00:00:00</h1>
+
+      <div className="buttons">
+        <button>Start</button>
+        <button>Resume</button>
+        <button>Stop</button>
+        <button>Lap</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
 };
 
 export default Stopwatch;
