5780c2f9ba535cf397d7c3806ddfe83e9df4e972
nzkks <kkshanthosh@gmail.com>
2024-09-29 23:36:30 +1300

    useFormatTime hook: simplify the calculation for easing the eyes

--- src/hooks/useFormatTime.ts
+++ src/hooks/useFormatTime.ts
@@ -1,9 +1,9 @@
 const useFormatTime = () => {
   const getFormattedTime = (time: number) => {
-    const hours = Math.floor(time / 3600000);
-    const minutes = Math.floor((time - hours * 3600000) / 60000);
-    const seconds = Math.floor((time - hours * 3600000 - minutes * 60000) / 1000);
-    const milliSeconds = Math.floor((time - hours * 3600000 - minutes * 60000 - seconds * 1000) / 100);
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((time / (1000 * 60)) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
+    const milliSeconds = Math.floor((time % 1000) / 100);
 
     return {
       hours: hours < 10 ? `0${hours}` : `${hours}`,
