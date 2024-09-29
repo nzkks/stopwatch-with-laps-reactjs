53160d95e7afa0cb4628cf3cde6961a0ebb6060e
nzkks <kkshanthosh@gmail.com>
2024-09-29 21:21:13 +1300

    connect the buttons with state

--- src/components/stopwatch/Stopwatch.tsx
+++ src/components/stopwatch/Stopwatch.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
+
 const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
   return (
     <div>
       <h1>00:00:00:00</h1>
 
       <div className="buttons">
-        <button>Start</button>
-        <button>Resume</button>
-        <button>Stop</button>
-        <button>Lap</button>
-        <button>Reset</button>
+        {!isRunning && !time && <button onClick={() => setIsRunning(true)}>Start</button>}
+        {!isRunning && time > 0 && <button onClick={() => setIsRunning(true)}>Resume</button>}
+        {isRunning && <button onClick={() => setIsRunning(false)}>Stop</button>}
+        {/* {isRunning && <button>Lap</button>} */}
+        {!isRunning && time > 0 && <button onClick={() => setTime(0)}>Reset</button>}
       </div>
     </div>
   );
