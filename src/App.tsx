9ba7dde13619c883cc4b5b1d0e01db74613e400d
nzkks <kkshanthosh@gmail.com>
2024-09-29 20:49:47 +1300

    Create a blank Stopwatch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import { Stopwatch } from './components';
 
 const App = () => {
   return (
     <>
       <h3>Stopwatch with laps history</h3>
+
+      <Stopwatch />
     </>
   );
 };
